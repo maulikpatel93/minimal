228a2dfa883992272319219de56eb229b8f8cf16
maulikpatel93 <programmer93.dynamicdreamz@gmail.com>
2024-04-17 17:57:37 +0530

    updated

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,35 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import Inspect from 'vite-plugin-inspect'
 import checker from 'vite-plugin-checker';
-
+import react from '@vitejs/plugin-react-swc';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-      },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1'),
-      },
+    plugins: [
+        Inspect(),
+        react(),
+        checker({
+            eslint: {
+                lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+            },
+        }),
     ],
-  },
-  server: {
-    port: 3030,
-  },
-  preview: {
-    port: 3030,
-  },
+    resolve: {
+        alias: [
+            {
+                find: /^~(.+)/,
+                replacement: path.join(process.cwd(), 'node_modules/$1'),
+            },
+            {
+                find: /^src(.+)/,
+                replacement: path.join(process.cwd(), 'src/$1'),
+            },
+        ],
+    },
+    server: {
+        port: 3030,
+    },
+    preview: {
+        port: 3030,
+    },
 });
