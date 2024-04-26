14e2cdf69f88f33d67434afe44e2613c690e220d
ketul <programmer106.dynamicdreamz@gmail.com>
2024-04-26 10:04:30 +0530

    update

--- my-app/src/services/dropdown.service.js
+++ my-app/src/services/dropdown.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import jwt from "jsonwebtoken";
+
 
 const API_URL = import.meta.env.VITE_HOST_API;;
 const VITE_JWT_SECRET = import.meta.env.VITE_JWT_SECRET;
@@ -10,11 +10,11 @@ const usersroleoption = (values) => {
     for (let value in values) {
         formData.append(value, values[value]);
     }
-    const payload = { user_id: 123 };
+    // const payload = { user_id: 123 };
     // const token = jwt.sign(payload, VITE_JWT_SECRET);
-    const token = jwt.sign({ foo: 'bar' }, VITE_JWT_SECRET, { algorithm: 'HS256', allowInsecureKeySizes: true })
+    // const token = jwt.sign({ foo: 'bar' }, VITE_JWT_SECRET, { algorithm: 'HS256', allowInsecureKeySizes: true })
     // const token = jwt.sign({x:"!22"}, VITE_JWT_SECRET);
-    console.log('token', token);
+    // console.log('token', token);
     // const formData1 = new FormData();
     // formData1.append('access_token', token)
     return axios.post(API_URL + action,formData1, {
@@ -24,69 +24,8 @@ const usersroleoption = (values) => {
     });
 };
 
-const countryOptions = (values) => {
-    const action = `/countries/options`;
-
-    return axios.get(API_URL + action, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${values?.isToken}`
-        },
-    });
-};
-
-const subcategoryoptions = (values) => {
-    const action = `/subcategory/subcategories/${values.id}`;
-    const formData = new FormData();
-    for (let value in values) {
-        formData.append(value, values[value]);
-    }
-
-    return axios.post(API_URL + action, formData, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${values.isToken}`
-        },
-    });
-};
-
-const brandOptions = (values) => {
-    const action = `/brands/options`;
-    const formData = new FormData();
-    for (let value in values) {
-        formData.append(value, values[value]);
-    }
-
-    return axios.post(API_URL + action, formData, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${values.isToken}`
-        },
-    });
-};
-
-const relatableproduct = (values) => {
-    const action = `/product/related`;
-    const formData = new FormData();
-    for (let value in values) {
-        formData.append(value, values[value]);
-    }
-
-    return axios.post(API_URL + action, formData, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${values.isToken}`
-        },
-    });
-};
-
-
 const dropdownApiController = {
-    usersroleoption,
-    countryOptions,
-    subcategoryoptions,
-    brandOptions,
-    relatableproduct
+    usersroleoption
 };
 
 export default dropdownApiController;
