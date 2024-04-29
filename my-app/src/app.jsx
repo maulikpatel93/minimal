b32b8419ce9b8c3dee4f4a0c3e0da13528c89486
ketul <programmer106.dynamicdreamz@gmail.com>
2024-04-29 16:14:52 +0530

    update

--- my-app/src/app.jsx
+++ my-app/src/app.jsx
@@ -19,7 +19,7 @@ import { MotionLazy } from 'src/components/animate/motion-lazy';
 import SnackbarProvider from 'src/components/snackbar/snackbar-provider';
 import { SettingsDrawer, SettingsProvider } from 'src/components/settings';
 
-import { CheckoutProvider } from 'src/sections/checkout/context';
+// import { CheckoutProvider } from 'src/sections/checkout/context';
 
 import { AuthProvider } from 'src/auth/context/jwt';
 
@@ -66,12 +66,12 @@ export default function App() {
             <ThemeProvider>
               <MotionLazy>
                 <SnackbarProvider>
-                  <CheckoutProvider>
+                  {/* <CheckoutProvider> */}
                     <SettingsDrawer />
                     <ProgressBar />
                     <ToastContainer />
                     <Router />
-                  </CheckoutProvider>
+                  {/* </CheckoutProvider> */}
                 </SnackbarProvider>
               </MotionLazy>
             </ThemeProvider>
--- my-app/src/routes/sections/main.jsx
+++ my-app/src/routes/sections/main.jsx
@@ -16,17 +16,17 @@ const Page404 = lazy(() => import('src/pages/404'));
 const FaqsPage = lazy(() => import('src/pages/faqs'));
 const AboutPage = lazy(() => import('src/pages/about-us'));
 const ContactPage = lazy(() => import('src/pages/contact-us'));
-const PricingPage = lazy(() => import('src/pages/pricing'));
-const PaymentPage = lazy(() => import('src/pages/payment'));
+// const PricingPage = lazy(() => import('src/pages/pricing'));
+// const PaymentPage = lazy(() => import('src/pages/payment'));
 const ComingSoonPage = lazy(() => import('src/pages/coming-soon'));
 const MaintenancePage = lazy(() => import('src/pages/maintenance'));
 // PRODUCT
-const ProductListPage = lazy(() => import('src/pages/product/list'));
-const ProductDetailsPage = lazy(() => import('src/pages/product/details'));
-const ProductCheckoutPage = lazy(() => import('src/pages/product/checkout'));
-// BLOG
-const PostListPage = lazy(() => import('src/pages/post/list'));
-const PostDetailsPage = lazy(() => import('src/pages/post/details'));
+// const ProductListPage = lazy(() => import('src/pages/product/list'));
+// const ProductDetailsPage = lazy(() => import('src/pages/product/details'));
+// const ProductCheckoutPage = lazy(() => import('src/pages/product/checkout'));
+// // BLOG
+// const PostListPage = lazy(() => import('src/pages/post/list'));
+// const PostDetailsPage = lazy(() => import('src/pages/post/details'));
 
 // ----------------------------------------------------------------------
 
@@ -43,23 +43,23 @@ export const mainRoutes = [
       { path: 'about-us', element: <AboutPage /> },
       { path: 'contact-us', element: <ContactPage /> },
       { path: 'faqs', element: <FaqsPage /> },
-      {
-        path: 'product',
-        children: [
-          { element: <ProductListPage />, index: true },
-          { path: 'list', element: <ProductListPage /> },
-          { path: ':id', element: <ProductDetailsPage /> },
-          { path: 'checkout', element: <ProductCheckoutPage /> },
-        ],
-      },
-      {
-        path: 'post',
-        children: [
-          { element: <PostListPage />, index: true },
-          { path: 'list', element: <PostListPage /> },
-          { path: ':title', element: <PostDetailsPage /> },
-        ],
-      },
+      // {
+      //   path: 'product',
+      //   children: [
+      //     { element: <ProductListPage />, index: true },
+      //     { path: 'list', element: <ProductListPage /> },
+      //     { path: ':id', element: <ProductDetailsPage /> },
+      //     { path: 'checkout', element: <ProductCheckoutPage /> },
+      //   ],
+      // },
+      // {
+      //   path: 'post',
+      //   children: [
+      //     { element: <PostListPage />, index: true },
+      //     { path: 'list', element: <PostListPage /> },
+      //     { path: ':title', element: <PostDetailsPage /> },
+      //   ],
+      // },
     ],
   },
   {
@@ -71,8 +71,8 @@ export const mainRoutes = [
       </SimpleLayout>
     ),
     children: [
-      { path: 'pricing', element: <PricingPage /> },
-      { path: 'payment', element: <PaymentPage /> },
+      // { path: 'pricing', element: <PricingPage /> },
+      // { path: 'payment', element: <PaymentPage /> },
     ],
   },
   {
